e79917c20f967b073c5bfd4f001e78f66889d376
admin <admin@NESARA>
2017-05-26 05:43:26 +0530

    SDK Added

--- NewApp/MainActivity.cs
+++ NewApp/MainActivity.cs
@@ -5,6 +5,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace NewApp
 {
@@ -12,13 +15,16 @@ namespace NewApp
     public class MainActivity : Activity
     {
         int count = 1;
-
+        //comment
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+            //VS Mob Instrumentation Key
+            MobileCenter.Start("fe3977fd-a699-4c7f-b1f0-377fd711bcef",
+                   typeof(Analytics), typeof(Crashes));
 
             // Get our button from the layout resource,
             // and attach an event to it
